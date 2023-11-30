36e5772a666a179e299ccf640cd8bc99169796d5
chakradhar63 <vanarasi.chakradhar@iiitb.ac.in>
2023-11-30 13:02:47 +0530

    files

---
+++ backend/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+function connectDB(){
+
+    mongoose.connect('mongodb+srv://chakrivanarasi:123@cluster0.vbhdsq1.mongodb.net/' , {useUnifiedTopology: true , useNewUrlParser: true})
+
+    const connection = mongoose.connection
+
+    connection.on('connected' , ()=>{
+        console.log('Mongo DB Connection Successfull')
+    })
+
+    connection.on('error' , ()=>{
+        console.log('Mongo DB Connection Error')
+    })
+
+
+}
+
+connectDB()
+
+module.exports = mongoose
---
+++ frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import {Provider} from 'react-redux'
+
+require('dotenv').config();
+
+const mongoConnection = process.env.MONGO;
+const jwtSecret = process.env.JWT;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
