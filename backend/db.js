96ddb06c04cae632c35e0a0c6d267a57f09e59b4
chakradhar63 <141206381+chakradhar63@users.noreply.github.com>
2023-12-02 23:08:05 +0530

    MongoDB connection code file

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 
 function connectDB(){
-
     mongoose.connect('mongodb+srv://chakrivanarasi:123@cluster0.vbhdsq1.mongodb.net/' , {useUnifiedTopology: true , useNewUrlParser: true})
-
     const connection = mongoose.connection
-
     connection.on('connected' , ()=>{
         console.log('Mongo DB Connection Successfull')
     })
